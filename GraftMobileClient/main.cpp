2d5c52de02081d0962868144df552946bc554dc3
grafit7 <grafit77800877@gmail.com>
2017-08-19 14:52:07 +0300

    Added space between includes Qt classes and our classes, created walletClient and passed it to QML layer

--- GraftMobileClient/main.cpp
+++ GraftMobileClient/main.cpp
@@ -3,7 +3,9 @@
 #include <QZXing.h>
 #include <QQuickView>
 #include <QQmlContext>
+
 #include "core/productmodel.h"
+#include "core/graftwalletclient.h"
 
 int main(int argc, char *argv[])
 {
@@ -17,6 +19,8 @@ int main(int argc, char *argv[])
     engine.load(QUrl(QLatin1String("qrc:/pos/main.qml")));
 #endif
 #ifdef WALLET_BUILD
+    GraftWalletClient walletClient;
+    engine.rootContext()->setContextProperty("walletClient", &walletClient);
     engine.load(QUrl(QLatin1String("qrc:/wallet/main.qml")));
 #endif
     if (engine.rootObjects().isEmpty())
