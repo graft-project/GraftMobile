8801d687d9c483953924f5397438c5ec5c7485f1
tetiana.gissovska <tetiana.gissovska@vakoms.com.ua>
2017-10-10 14:18:55 +0300

    SplashActivity.java onlu created activity, added connection splash.xml. In it set two picher(logo and background) witch was add all formats.

--- GraftMobileClient/main.cpp
+++ GraftMobileClient/main.cpp
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
         return -1;
 
 #ifdef Q_OS_ANDROID
-    //QtAndroid::hideSplashScreen();
+    QtAndroid::hideSplashScreen();
 #endif
     return app.exec();
 }
