60b10fd1ee40392a3e95b08048693e56a5a814f9
Dmytro Mykuliak <dmytro.mykuliak@vakoms.com.ua>
2017-10-13 19:46:52 +0300

    Made border in QR code less.

--- GraftMobileClient/core/qrcodegenerator.cpp
+++ GraftMobileClient/core/qrcodegenerator.cpp
@@ -10,6 +10,6 @@ QRCodeGenerator::QRCodeGenerator()
 QImage QRCodeGenerator::encode(const QString &message)
 {
     qrcodegen::QrCode qrcode = qrcodegen::QrCode::encodeText(message.toStdString().c_str(), qrcodegen::QrCode::Ecc::QUARTILE);
-    QImage img = QImage::fromData(QByteArray::fromStdString(qrcode.toSvgString(4)));
+    QImage img = QImage::fromData(QByteArray::fromStdString(qrcode.toSvgString(2)));
     return img.scaled(300, 300);
 }
