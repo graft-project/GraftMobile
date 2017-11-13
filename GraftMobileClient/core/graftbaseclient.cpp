4e889f99f2d0d654e8f260d86666858bf8de0492
Dmytro Mykuliak <dmytro.mykuliak@vakoms.com.ua>
2017-11-13 20:38:38 +0200

    Stored client settings.

--- GraftMobileClient/core/graftbaseclient.cpp
+++ GraftMobileClient/core/graftbaseclient.cpp
@@ -1,17 +1,25 @@
 #include "barcodeimageprovider.h"
 #include "quickexchangemodel.h"
 #include "graftbaseclient.h"
+#include <QStandardPaths>
 #include <QQmlEngine>
+#include <QSettings>
+#include <QFileInfo>
+#include <QString>
+#include <QDir>
 
 static const QString cBarcodeImageProviderID("barcodes");
 static const QString cQRCodeImageID("qrcode");
 static const QString cProviderScheme("image://%1/%2");
+static const QString scSettingDataFile("Settings.ini");
 
 GraftBaseClient::GraftBaseClient(QObject *parent)
     : QObject(parent)
     ,mImageProvider(nullptr)
+    ,mClientSettings(nullptr)
     ,mQuickExchangeModel(new QuickExchangeModel(this))
 {
+    initSettings();
 }
 
 void GraftBaseClient::setQRCodeImage(const QImage &image)
@@ -39,4 +47,30 @@ void GraftBaseClient::registerImageProvider(QQmlEngine *engine)
 QuickExchangeModel *GraftBaseClient::quickExchangeModel() const
 {
     return mQuickExchangeModel;
+}
+
+void GraftBaseClient::setSettings(const QString &key, const QVariant &value)
+{
+    mClientSettings->setValue(key,value);
+}
+
+QVariant GraftBaseClient::settings(const QString &key) const
+{
+    return mClientSettings->value(key);
+}
+
+void GraftBaseClient::saveSettings() const
+{
+    mClientSettings->sync();
+}
+
+void GraftBaseClient::initSettings()
+{
+    QString dataPath = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    if (!QFileInfo(dataPath).exists())
+    {
+        QDir().mkpath(dataPath);
+    }
+    QDir lDir(dataPath);
+    mClientSettings = new QSettings(lDir.filePath(scSettingDataFile), QSettings::IniFormat, this);
 }
--- GraftMobileClient/core/graftbaseclient.h
+++ GraftMobileClient/core/graftbaseclient.h
@@ -2,10 +2,12 @@
 #define GRAFTBASECLIENT_H
 
 #include <QObject>
+#include <QVariant>
 
 class BarcodeImageProvider;
 class QuickExchangeModel;
 class QQmlEngine;
+class QSettings;
 
 class GraftBaseClient : public QObject
 {
@@ -20,12 +22,18 @@ public:
 
     QuickExchangeModel *quickExchangeModel() const;
 
+    void initSettings();
+    Q_INVOKABLE void saveSettings() const;
+    Q_INVOKABLE QVariant settings(const QString &key) const;
+    Q_INVOKABLE void setSettings(const QString &key, const QVariant &value);
+
 signals:
     void errorReceived();
 
 protected:
     BarcodeImageProvider *mImageProvider;
     QuickExchangeModel *mQuickExchangeModel;
+    QSettings *mClientSettings;
 };
 
 #endif // GRAFTBASECLIENT_H
