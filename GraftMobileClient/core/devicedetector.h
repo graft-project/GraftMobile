6fe7debc668ab37642633287811dae49e661bde6
Dmytro Mykuliak <dmytro.mykuliak@vakoms.com.ua>
2018-01-19 20:21:15 +0200

    Added a few iPhone devices, and added static to detectDevice().

--- GraftMobileClient/core/devicedetector.h
+++ GraftMobileClient/core/devicedetector.h
@@ -16,6 +16,12 @@ public:
         iPadPro12_9,
         iPadPro10_5,
         iPad,
+        iPhone6S = iPhoneNormal,
+        iPhone7 = iPhoneNormal,
+        iPhone8 = iPhoneNormal,
+        iPhone6SPlus = iPhonePlus,
+        iPhone7Plus = iPhonePlus,
+        iPhone8Plus = iPhonePlus,
         iPad9_7 = iPad,
         iPad7_9 = iPad
     };
@@ -24,7 +30,7 @@ public:
     DeviceDetector(QObject *parent = nullptr);
 
     void registerTypes(QQmlEngine *engine);
-    Q_INVOKABLE int detectDevice();
+    Q_INVOKABLE static int detectDevice();
 };
 
 #endif // DEVICEDETECTOR_H
