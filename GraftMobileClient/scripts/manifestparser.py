bfbce77c39e8fbcb1d4f7c38f56ae7b0e9751ee8
Dmytro Mykuliak <dmytro.mykuliak@vakoms.com.ua>
2018-05-24 21:38:20 +0300

    Refactored script for parsing Android manifest file.

--- GraftMobileClient/scripts/manifestparser.py
+++ GraftMobileClient/scripts/manifestparser.py
@@ -1,19 +1,17 @@
 from xml.dom import minidom
 
 def setVersionCode(majorVersion = 0, minorVersion = 0, buildVersion = 0):    
+    lMinorVersion = 0
     if minorVersion >= 10 and minorVersion <= 99:
         lMinorVersion = minorVersion
     elif minorVersion < 10:
-        lMinorVersion = "{}{}".format(0, minorVersion)
-    else:
-        lMinorVersion = 0
+        lMinorVersion = "0{}".format(minorVersion)
 
+    lBuildVersion = 0
     if buildVersion >= 10 and buildVersion <= 99:
         lBuildVersion = buildVersion
     elif buildVersion < 10:
-        lBuildVersion = "{}{}".format(0, buildVersion)
-    else:
-        lBuildVersion = 0
+        lBuildVersion = "0{}".format(buildVersion)
 
     return "{}{}{}".format(majorVersion, lMinorVersion, lBuildVersion)
 
@@ -23,16 +21,9 @@ def changeVersion(pathToFile, majorVersion = 0, minorVersion = 0, buildVersion =
     versionName = "{}.{}.{}".format(majorVersion, minorVersion, buildVersion)
     versionCode = setVersionCode(majorVersion, minorVersion, buildVersion)
 
-    print(root.documentElement.getAttribute("android:versionName"))
-    print(root.documentElement.getAttribute('android:versionCode'))
-
     root.documentElement.setAttribute("android:versionName", versionName)
     root.documentElement.setAttribute("android:versionCode", versionCode)
 
-    print(root.documentElement.getAttribute("android:versionName"))
-    print(root.documentElement.getAttribute('android:versionCode'))
-
-    file = open("C:\\Users\\dmytr\\Desktop\\Python\\android.xml", "wb")
-    print(root.toxml().encode('utf-8'))
-    file.write(root.toxml().encode('utf-8'))
-    file.close()
+    with open(pathToFile, "wb") as file:
+        file.write(root.toxml().encode('utf-8'))
+        file.close()
