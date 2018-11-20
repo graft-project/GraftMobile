875d6857c294796366deb96bb8dc5c182d32d1a3
Tetiana Gissovska <tetiana.gissovska@vakoms.com.ua>
2018-11-20 12:08:19 +0200

    Update graftwallethandlerv2.cpp

--- GraftMobileClient/core/api/v2/graftwallethandlerv2.cpp
+++ GraftMobileClient/core/api/v2/graftwallethandlerv2.cpp
@@ -123,7 +123,7 @@ void GraftWalletHandlerV2::transfer(const QString &address, const QString &amoun
     if (mWallet)
     {
         bool status = mWallet->sendCurrentTransaction();
-        emit transferReceived(status); //BUG types is different (int, bool)
+        emit transferReceived(status);
     }
 }
 
@@ -144,7 +144,7 @@ void GraftWalletHandlerV2::saleDetails(const QString &pid, int blockNumber)
 void GraftWalletHandlerV2::rejectPay(const QString &pid, int blockNumber)
 {
     //TODO: Fix support of rejectPay, unsupported on supernode side
-//    mApi->rejectPay(mPID, mBlockNumber);
+    //mApi->rejectPay(mPID, mBlockNumber);
 }
 
 void GraftWalletHandlerV2::pay(const QString &pid, const QString &address, double amount,
