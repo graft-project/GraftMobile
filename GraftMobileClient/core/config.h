35d4f6c405f3f860ce9c288b226d2eabe6d1e90e
Ilya Kitaev <mbg033@gmail.com>
2020-03-09 19:58:22 +0300

    network config: supernode port changed to new supernode for public testnet

--- GraftMobileClient/core/config.h
+++ GraftMobileClient/core/config.h
@@ -12,9 +12,6 @@ static const QString scConfigTitle("Mainnet");
 
 static const QStringList scHttpSeedSupernodes{
                                               "mainnet-seed.graft.network:18900"
-                                              //"13.58.215.50:18900",
-                                              //"13.59.105.220:18900",
-                                              //"18.216.94.64:18900"
                                              };
 
 static const QStringList scHttpsSeedSupernodes{"mainnet-seed.graft.network:18943"};
@@ -27,13 +24,10 @@ namespace TestnetConfiguration
 static const QString scConfigTitle("Public Testnet");
 
 static const QStringList scHttpSeedSupernodes{
-                                              "testnet-pub-seed.graft.network:28900"
-                                              //"18.214.245.162:28900",
-                                              //"52.204.250.197:28900",
-                                              //"34.225.70.106:28900"
+                                              "testnet-pub-seed.graft.network:28690"
                                              };
 
-static const QStringList scHttpsSeedSupernodes{"testnet-pub-seed.graft.network:28943"};
+static const QStringList scHttpsSeedSupernodes{"testnet-pub-seed.graft.network:28643"};
 
 static const QString scDAPIVersion("1.0F");
 }
@@ -42,22 +36,12 @@ namespace ExperimentalTestnetConfiguration
 {
 static const QString scConfigTitle("Public Devnet");
 
+
 static const QStringList scHttpSeedSupernodes{
-                                              "testnet-dev-seed.graft.network:28600"
-                                              //"18.214.197.224:28690"
-                                              //"18.214.197.50:28690"
-                                              //"35.169.179.171:28690"
-                                              //"34.192.115.160:28690"
-                                              //"testnet-rta-seed.graft.network:28900"
-                                              //"34.200.186.98:28900",
-                                              //"34.224.118.182:28900",
-                                              //"52.206.105.250:28900"
+                                              "testnet-pub-seed.graft.network:28690"
                                              };
 
-static const QStringList scHttpsSeedSupernodes{
-                                               "testnet-dev-seed.graft.network:28643"
-                                               //"testnet-rta-seed.graft.network:28943"
-                                              };
+static const QStringList scHttpsSeedSupernodes{"testnet-pub-seed.graft.network:28643"};
 
 static const QString scDAPIVersion("1.0R");
 }
